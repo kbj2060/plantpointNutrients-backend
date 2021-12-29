e900c74b8fd2ad83f01da4365fad04d51a48b5dd
kbj2060 <kbj2060@naver.com>
2021-12-29 23:49:44 +0900

    [New Feature] main updated

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import uvicorn
-
+from controllers.app import app
 from controllers.mqtt import *
 from controllers.machines import *
 from controllers.temperature import *
@@ -34,4 +34,4 @@ from controllers.auth import *
 #########################
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info")
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info", workers=2, reload=True)
